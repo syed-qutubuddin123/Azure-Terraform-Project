6425abbc2b211c5023accb271a1603332a36e660
root <root@Terraform-server.a4orhu1jlq2uvjyyg5xvn52n0d.tx.internal.cloudapp.net>
2025-12-03 11:18:43 +0000

    azure-terraform

--- main.tf
+++ main.tf
@@ -17,43 +17,11 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "my_nic" {
-  for_each            = var.servers
-  name                = "my_nic-${each.key}"
+module "vm" {
+  source              = "./modules/vm"
+  servers             = var.servers
   location            = var.location
   resource_group_name = azurerm_resource_group.my_rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "my_vm" {
-  for_each            = var.servers
-  name                = each.key
-  resource_group_name = azurerm_resource_group.my_rg.name
-  location            = var.location
-  size                = each.value
-  admin_username      = "syedazure"
-  admin_password      = "Password@123"
-  disable_password_authentication = false
-
-  network_interface_ids = [
-    azurerm_network_interface.my_nic[each.key].id
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-    source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
+  subnet_id           = azurerm_subnet.subnet.id
 }
 
